0663be796a9fb313363b14c143bc8c150b9bc27c
zabrla <isabelaaseidler@gmail.com>
2026-09-16 15:30:31 -0300

    arquivos p1 fullstack

---
+++ acervo/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Exemplar(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    codigo_registro = models.CharField(max_length=50)
+    disponivel = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.livro.titulo} - Cód: {self.codigo_registro}"
+
+class Membro(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+class Emprestimo(models.Model):
+    exemplar = models.ForeignKey(Exemplar, on_delete=models.CASCADE)
+    membro = models.ForeignKey(Membro, on_delete=models.CASCADE)
+    data_emprestimo = models.DateField()
+    data_devolucao_prevista = models.DateField()
+    data_devolucao_real = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"Empréstimo: {self.exemplar.livro.titulo} para {self.membro.nome}"
+
+class Reserva(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    membro = models.ForeignKey(Membro, on_delete=models.CASCADE)
+    data_reserva = models.DateField()
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Reserva: {self.livro.titulo} por {self.membro.nome}"
